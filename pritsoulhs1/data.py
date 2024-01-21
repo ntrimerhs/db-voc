be827e9b4a061eb0d0b336b11f838516c88c7c8c
ntrimerhs <75494938+ntrimerhs@users.noreply.github.com>
2024-01-21 20:14:48 +0200

    typo

--- pritsoulhs1/data.py
+++ pritsoulhs1/data.py
@@ -1,7 +1,7 @@
 import sys
 from openpyxl import Workbook, load_workbook
 
-wb = load_workbook('./../excel/pritsoulhs2/pritsoulhs1.xlsx') 
+wb = load_workbook('./../excel/pritsoulhs1/pritsoulhs1.xlsx') 
 ws = wb.active
 
 kales = int(sys.argv[1])
@@ -19,5 +19,5 @@ ws['E5'] = kakes
 ws['G5'] = epistrofes
 ws['H5'] = sxolia
 
-wb.save('./../excel/pritsoulhs2/pritsoulhs1.xlsx') 
+wb.save('./../excel/pritsoulhs1/pritsoulhs1.xlsx') 
 sys.stdout.flush()
