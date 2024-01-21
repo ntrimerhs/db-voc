fa1317729526ec211a87a649a4caacfbadbf6a7e
ntrimerhs <75494938+ntrimerhs@users.noreply.github.com>
2024-01-21 22:32:23 +0200

    details

--- firebase-mega/data.py
+++ firebase-mega/data.py
@@ -8,6 +8,6 @@ sxolia = int(sys.argv[12])
 
 f=open("output.txt", "a")
 print(type(kales), type(metries), type(kakes), type(epistrofes), type(sxolia))
-print(kales, metries, kakes, epistrofes, sxolia, file=f)
+print("Καλές: ", kales, "Μέτριες", metries, "Κακές", kakes, "Επιστροφή-Στο-Κατάστημα", epistrofes, "Σχόλια", sxolia, file=f)
 
 sys.stdout.flush()
