cd21fbe261e71c4d2f301f4a734b1bd49aa9a040
ntrimerhs <75494938+ntrimerhs@users.noreply.github.com>
2024-01-21 20:03:15 +0200

    fix

--- firebase-mega/data.py
+++ firebase-mega/data.py
@@ -1,7 +1,7 @@
 import sys
 from openpyxl import Workbook, load_workbook
 
-wb = load_workbook('./../excel/mega/mega.xlsm') 
+wb = load_workbook('./../excel/mega/mega.xlsx') 
 ws = wb.active
 
 kales = int(sys.argv[1])
@@ -19,5 +19,5 @@ ws['E5'] = kakes
 ws['G5'] = epistrofes
 ws['H5'] = sxolia
 
-wb.save('./../excel/mega/mega.xlsm') 
+wb.save('./../excel/mega/mega.xlsx') 
 sys.stdout.flush()
--- pritsoulhs1/data.py
+++ pritsoulhs1/data.py
@@ -1,7 +1,7 @@
 import sys
 from openpyxl import Workbook, load_workbook
 
-wb = load_workbook('./../excel/pritsoulhs2/pritsoulhs2.xlsm') 
+wb = load_workbook('./../excel/pritsoulhs2/pritsoulhs2.xlsx') 
 ws = wb.active
 
 kales = int(sys.argv[1])
@@ -19,5 +19,5 @@ ws['E5'] = kakes
 ws['G5'] = epistrofes
 ws['H5'] = sxolia
 
-wb.save('./../excel/pritsoulhs2/pritsoulhs2.xlsm') 
+wb.save('./../excel/pritsoulhs2/pritsoulhs2.xlsx') 
 sys.stdout.flush()
