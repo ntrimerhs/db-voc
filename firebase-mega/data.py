767b0cb1e4af4ddb706fd0eecb116d6c6b136b6e
ntrimerhs <75494938+ntrimerhs@users.noreply.github.com>
2024-01-21 20:30:31 +0200

    new

--- firebase-mega/data.py
+++ firebase-mega/data.py
@@ -1,8 +1,4 @@
 import sys
-from openpyxl import Workbook, load_workbook
-
-wb = load_workbook('./../excel/mega/mega.xlsx') 
-ws = wb.active
 
 kales = int(sys.argv[1])
 metries = int(sys.argv[2])
@@ -10,14 +6,8 @@ kakes = int(sys.argv[3])
 epistrofes = int(sys.argv[10])
 sxolia = int(sys.argv[12])
 
+f=open("output.txt", "a")
 print(type(kales), type(metries), type(kakes), type(epistrofes), type(sxolia))
-print(kales, metries, kakes, epistrofes, sxolia)
+print(kales, metries, kakes, epistrofes, sxolia, file=f)
 
-ws['C5'] = kales 
-ws['D5'] = metries
-ws['E5'] = kakes 
-ws['G5'] = epistrofes
-ws['H5'] = sxolia
-
-wb.save('./../excel/mega/mega.xlsx') 
 sys.stdout.flush()
